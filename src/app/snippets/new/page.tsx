0837ccdfbd5d35da3cd4573271d888a44ce4fc89
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-19 16:59:44 +0330

    create new snippet route

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateSnippets() {
+  return (
+    <h3 className="max-w-[350px] text-slate-800 mr-8 font-semibold text-[32px] text-center">
+      +ساخت قطعه کد جدید
+    </h3>
+  );
+}
