7f74b9dc1ccedc2698cc5a7928f82c99dd86070a
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-21 11:21:54 +0330

    feat:implement server action for creating codeSnippets

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,13 +1,41 @@
+import { redirect } from 'next/navigation';
+
+import { db } from '@/db';
 import { Input, Button } from '@/components';
 
 export default function CreateSnippets() {
+  const createCodeSnippets = async (formData: FormData) => {
+    //  server action
+    'use server';
+
+    // get title and code snippets from form
+    const title = formData.get('title') as string;
+    const codeSnippet = formData.get('codeSnippet') as string;
+
+    // create data into database and log the data
+    const snippets = await db.snippets.create({
+      data: {
+        title,
+        codeSnippet,
+      },
+    });
+
+    console.log(snippets);
+
+    // redirect to root
+    redirect('/');
+  };
+
   return (
     <section>
       <h3 className="max-w-[350px] text-slate-800 mr-8 font-semibold text-[32px] text-center">
         +ساخت قطعه کد جدید
       </h3>
 
-      <form className="flex flex-col justify-center max-w-[470px] gap-3 px-[72px] mt-10">
+      <form
+        className="flex flex-col justify-center max-w-[470px] gap-3 px-[72px] mt-10"
+        action={createCodeSnippets}
+      >
         <Input type="text" placeholder="عنوان" style="w-full" title="title" />
 
         <label htmlFor="codeSnippet">
