508a8130ee7c8f86ffda1057b296e2953d42d969
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-21 12:23:14 +0330

    fetch data from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div>صفحه اصلی</div>;
+import { db } from '@/db';
+
+export default async function HomePage() {
+  const snippets = await db.snippets.findMany();
+
+  const renderedSnippets = snippets.map(({ id, title, codeSnippet }) => (
+    <div key={id}>
+      <h3>{title}</h3>
+      <p>{codeSnippet}</p>
+    </div>
+  ));
+
+  return <div>{renderedSnippets}</div>;
 }
