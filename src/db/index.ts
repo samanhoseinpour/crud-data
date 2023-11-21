dabad52cb8c549915c6499ad846a588feddf7e79
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-21 11:21:11 +0330

    fix: delete boilerplate

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,27 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
 export const db = new PrismaClient();
-
-const main = async () => {
-  const sinppets = await db.snippets.create({
-    data: {
-      title: 'NextJS Snippet',
-      codeSnippet: `export default asyc function getSnippets() {
-        // return something else 
-      }`,
-    },
-  });
-
-  console.log(sinppets);
-};
-
-main()
-  .then(async () => {
-    await db.$disconnect();
-  })
-  .catch(async (e) => {
-    console.log(e);
-
-    await db.$disconnect();
-    process.exit(1);
-  });
