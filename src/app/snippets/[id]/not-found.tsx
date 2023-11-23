5a6ce3bc2f39fa9e51c0ef3ec0e3b39e91b69da7
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-23 11:38:26 +0330

    fix:404 snippets id

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -2,13 +2,18 @@ import Link from 'next/link';
 
 export default function SnippetNotFound() {
   return (
-    <div className="flex-col justify-center items-center bg-gradient-to-r px-12 py-24 rounded-full text-center text-blue-900">
+    <div className="flex-col justify-center items-center bg-gray-100 px-12 py-24 rounded-full text-center text-blue-900">
       <h1 className="text-xl font-bold  ">
         متأسفیم! اما نتوانستیم قطعه کدی را که به دنبال آن هستید، پیدا کنیم
       </h1>
 
-      <div className="mt-10 font-bold">
-        <Link href="/">بازگشت</Link>
+      <div className="mt-10 font-bold ">
+        <Link
+          href="/"
+          className="border-1 rounded-full bg-blue-950 text-white px-4 py-2"
+        >
+          بازگشت
+        </Link>
       </div>
     </div>
   );
